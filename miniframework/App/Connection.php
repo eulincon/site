1c9651123a116ce14e1d062a6866d79dd80ebc71
zlincon <38511541+zlincon@users.noreply.github.com>
2020-01-09 15:41:03 -0300

    Update Connection.php

--- miniframework/App/Connection.php
+++ miniframework/App/Connection.php
@@ -5,14 +5,14 @@
 		public static function getDb() {
 			try{
 				$conn = new \PDO(
-					"mysql:host=localhost;dbname=mvc;charset=utf8",
-					"root",
-					"" 
+					'mysql:host=localhost;dbname=php_com_pdo',
+					'root',
+					''
 				);
-
 				return $conn;
 			}catch(\PDOException $e){
 				//Tratar erros
+				echo 'Error: '.$e->getMessage();
 			}
 		}
 	}
