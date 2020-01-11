f1fdffe47116d453221b3bfa32775b44ee93780b
zlincon <38511541+zlincon@users.noreply.github.com>
2020-01-11 11:11:22 -0300

    #

---
+++ miniframework/App/Models/Usuario.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+	private $id;
+	private $nome;
+	private $email;
+	private $senha;
+
+
+	//validar se um cadastro pode ser feito
+
+	//recuperar um usuario por email
+
+	public function __get($attr){
+		return $this->$attr;
+	}
+
+	public function __set($attr, $value){
+		$this->$attr = $value;
+	}
+
+	//salvar
+	public function salvar(){
+		$query = "insert into usuarios(nome, email, senha) values(:nome, :email, :senha)";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':nome', $this->__get('nome'));
+		$stmt->bindValue(':email', $this->__get('email'));
+		$stmt->bindValue(':senha', $this->__get('senha')); //metodo md5 para crisptografia
+		$stmt->execute();
+
+		return $this;
+	}
+
+	public function validarCadastro(){
+		$valido = true;
+
+		if(strlen($this->__get('nome')) < 3){
+			$valido = false;
+		}
+		if(strlen($this->__get('email')) < 3){
+			$valido = false;
+		}
+		if(strlen($this->__get('senha')) < 3){
+			$valido = false;
+		}
+
+		return $valido;
+	}
+
+	public function getUsuarioPorEmail(){
+		$query = "select nome, email from usuarios where email = :email";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':email', $this->__get('email'));
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function autenticar(){
+		$query = "select id, nome, email from usuarios where email = :email and senha =:senha";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':email', $this->__get('email'));
+		$stmt->bindValue(':senha', $this->__get('senha'));
+		$stmt->execute();
+
+		$usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+		if($usuario['id'] != '' && $usuario['nome'] != ''){
+			$this->__set('id', $usuario['id']);
+			$this->__set('nome', $usuario['nome']);
+		}
+
+		return $this;
+	}
+}
+
+?>
--- miniframework/App/Route.php
+++ miniframework/App/Route.php
@@ -14,10 +14,34 @@ class Route extends Bootstrap {
 			'action' => 'index'
 		);
 
-		$routes['sobre_nos'] = array(
-			'route' => '/sobre_nos',
+		$routes['inscreverse'] = array(
+			'route' => '/inscreverse',
 			'controller' => 'indexController',
-			'action' => 'sobreNos'
+			'action' => 'inscreverse'
+		);
+
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
+
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
 		);
 
 		$this->setRoutes($routes);
